856bc92a81944ae7500c38e54e7dacfbca4b949f
Jacob Norgren <jacobnorgren3@gmail.com>
2020-04-21 14:09:01 +0200

    jsak

--- Chattprogram/Game1.cs
+++ Chattprogram/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Chattprogram
 {
@@ -12,7 +14,73 @@ namespace Chattprogram
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Random random = new Random();
+
+        SpriteFont normalText;
+
         Texture2D chattWindow;
+
+        bool taken = true;
+
+        int time = 0;
+        int second = 0;
+        int minute = 0;
+
+        List<string> funfacts;
+
+        List<Vector2> positions;
+
+        List<int> randomized;
+
+        List<int> takenList = new List<int>();
+
+        public void Funfacts(SpriteBatch sb)
+        {
+            if (second % 10 == 0 && second > 0)
+            {
+                second = 0;
+                while (taken == true)
+                {
+                    takenList.Clear();
+
+                    taken = false;
+
+                    randomized[0] = random.Next(0, 18);
+                    takenList.Add(randomized[0]);
+
+                    for (int i = 1; i < 4; i++)
+                    {
+                        randomized[i] = random.Next(0, 18);
+
+                        if (randomized[i] == takenList[i - 1])
+                        {
+                            taken = true;
+                            break;
+                        }
+                        else
+                        {
+                            takenList.Add(randomized[i]);
+                        }
+                        
+                    }
+
+                }
+
+                
+            }
+
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                sb.DrawString(normalText, funfacts[randomized[i]], positions[i], Color.Black);
+            }
+
+            
+
+        }
+
+
 
         public Game1()
         {
@@ -22,12 +90,7 @@ namespace Chattprogram
             graphics.PreferredBackBufferHeight = 600;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -37,34 +100,44 @@ namespace Chattprogram
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            normalText = Content.Load<SpriteFont>("normalText");
+
+            randomized = new List<int> { random.Next(0, 18), random.Next(0, 18), random.Next(0, 18),
+            random.Next(0, 18)};
+
             chattWindow = Content.Load<Texture2D>("vitt");
+
+            funfacts = new List<string> { "Peppa pig is 7 feet tall", "George W Bush invaded my \nkitchen for oil",
+            "The Corona virus caused a whole \nnew meaning of stranger danger", "Cooking a bat lead to the \nshortage of toilet paper in \nthe US",
+            "Handing out your Bank Account \ndetails may lead to devastating \nconsequences", "If your body could digest \nuranium consuming one gram of \nit would make you 2267 kg \nheavier",
+            "People are catfished around the \nworld more than 1800 times per \nmonth, stay safe", "Kik, Omegle, Whatsapp, \nMessenger and Snapchat \nhas herpes",
+            "Once a kid got a triple kill in cod \nand asked his mom to get the \ncamera", "Be nice chatting in this forum, \notherwise you might be sent to \nthe gulag",
+            "Successful men like Bezos and \nBill Gates have killed on \nnumerous occasions", "THERZZ A BOMB IN ZERE \nsaid Arnold Schwarznegger",
+            "There are no trustworthy \npersons in the White House, \nthey are all snakes", "The Ranch has recovered more \npersons than all hospitals put \ntogether",
+            "Never give out personal \ninformation to anyone, \neven if it is Indian \ntech support", "Mike Tyson was once offered to \nbox Putin for 80 000 000 dollars, \nhe never accepted",
+            "Milk is cereal sauce, \nchange my mind", "Poop are dense farts", "WAZUP"};
+
+            positions = new List<Vector2> { new Vector2(5, 180), new Vector2(5, 380), new Vector2(805, 180), 
+            new Vector2(805, 380)};
+
             
 
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
+
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
@@ -76,9 +149,7 @@ namespace Chattprogram
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
+
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
@@ -87,6 +158,20 @@ namespace Chattprogram
             spriteBatch.Begin();
 
             spriteBatch.Draw(chattWindow, new Vector2(200, 100), Color.White);
+
+            spriteBatch.DrawString(normalText, time + " " + second, new Vector2(50, 30), Color.Black);
+
+            time += 1;
+
+            if (time % 60 == 0)
+            {
+                second += 1;
+                time = 0;
+            }
+
+            Funfacts(spriteBatch);
+
+
 
             spriteBatch.End();
 
